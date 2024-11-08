66c237e6e86aabedb86b8b58d64806e918b3fc84
dmurawski <dmurawski018@gmail.com>
2024-11-08 14:20:00 +0100

    add detail and fix urls in templates

--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,28 @@
-from django.http import HttpResponse
 from .models import Question
-from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    print(latest_question_list)
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, id):
-    return HttpResponse(f"question {id}")
+    # try:
+    #     question = Question.objects.get(pk=id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    # Skrót do tego co powyżej
+    question = get_object_or_404(Question, pk=id)
+
+    context = {
+        "question": question,
+    }
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, id):
